7040c51acf59ce8d5f41b513132e8d85fc9f5435
Joaco_Vrz <joacoe.vrz@gmail.com>
2026-04-07 02:49:23 -0600

    Estructura para pagina de servicios dinamica en proceso (WIP)
    Se añadio un archivo para los servicios organizados por categorias + el archivo para la pagina dinamica de servicios y se hicieron pequeños ajustes al diseño de la pagina info

---
+++ src/data/servicios.js
@@ -0,0 +1,346 @@
+export const servicios = [
+    // * ------ Servicio Contable ------
+    {
+        id: 1.1,
+        category: "servicio-contable",
+        title: "Servicio Contable",
+        icon: "",
+        description: ""
+    },
+    {
+        id: 1.2,
+        category: "servicio-contable",
+        title: "Servicio Contable",
+        icon: "",
+        description: ""
+    },
+    {
+        id: 1.3,
+        category: "servicio-contable",
+        title: "Servicio Contable",
+        icon: "",
+        description: ""
+    },
+    {
+        id: 1.4,
+        category: "servicio-contable",
+        title: "Servicio Contable",
+        icon: "",
+        description: ""
+    },
+    {
+        id: 1.5,
+        category: "servicio-contable",
+        title: "Servicio Contable",
+        icon: "",
+        description: ""
+    },
+    {
+        id: 1.6,
+        category: "servicio-contable",
+        title: "Servicio Contable",
+        icon: "",
+        description: ""
+    },
+    // * ------ Servicio Fiscal ------
+    {
+        id: 2.1,
+        category: "servicio-fiscal",
+        icon: "",
+        title: "Servicio Fiscal",
+        description: ""
+    },
+    {
+        id: 2.2,
+        category: "servicio-fiscal",
+        title: "Servicio Fiscal",
+        icon: "",
+        description: ""
+    },
+    {
+        id: 2.3,
+        category: "servicio-fiscal",
+        title: "Servicio Fiscal",
+        icon: "",
+        description: ""
+    },
+    {
+        id: 2.4,
+        category: "servicio-fiscal",
+        title: "Servicio Fiscal",
+        icon: "",
+        description: ""
+    },
+    {
+        id: 2.5,
+        category: "servicio-fiscal",
+        title: "Servicio Fiscal",
+        icon: "",
+        description: ""
+    },
+    {
+        id: 2.6,
+        category: "servicio-fiscal",
+        title: "Servicio Fiscal",
+        icon: "",
+        description: ""
+    },
+    {
+        id: 3.1,
+        category: "capacitacion",
+        icon: "",
+        title: "Capacitación",
+        description: ""
+    },
+    // * ------ Capacitación ------
+    {
+        id: 3.2,
+        category: "capacitacion",
+        icon: "",
+        title: "Capacitación",
+        description: ""
+    },
+    {
+        id: 3.3,
+        category: "capacitacion",
+        icon: "",
+        title: "Capacitación",
+        description: ""
+    },
+    {
+        id: 3.4,
+        category: "capacitacion",
+        icon: "",
+        title: "Capacitación",
+        description: ""
+    },
+    {
+        id: 3.5,
+        category: "capacitacion",
+        icon: "",
+        title: "Capacitación",
+        description: ""
+    },
+    {
+        id: 3.6,
+        category: "capacitacion",
+        icon: "",
+        title: "Capacitación",
+        description: ""
+    },
+    // * ------ Recursos Humanos ------
+    {
+        id: 4.1,
+        category: "recursos-humanos",
+        icon: "",
+        title: "Recursos Humanos",
+        description: ""
+    },
+    {
+        id: 4.2,
+        category: "recursos-humanos",
+        icon: "",
+        title: "Recursos Humanos",
+        description: ""
+    },
+    {
+        id: 4.3,
+        category: "recursos-humanos",
+        icon: "",
+        title: "Recursos Humanos",
+        description: ""
+    },
+    {
+        id: 4.4,
+        category: "recursos-humanos",
+        icon: "",
+        title: "Recursos Humanos",
+        description: ""
+    },
+    {
+        id: 4.5,
+        category: "recursos-humanos",
+        icon: "",
+        title: "Recursos Humanos",
+        description: ""
+    },
+    {
+        id: 4.6,
+        category: "recursos-humanos",
+        icon: "",
+        title: "Recursos Humanos",
+        description: ""
+    },
+    // * ------ Servicios para Trabajadores ------
+    {
+        id: 5.1,
+        category: "servicios-para-trabajadores",
+        icon: "",
+        title: "Servicios para trabajadores",
+        description: ""
+    },
+    {
+        id: 5.2,
+        category: "servicios-para-trabajadores",
+        icon: "",
+        title: "Servicios para trabajadores",
+        description: ""
+    },
+    {
+        id: 5.3,
+        category: "servicios-para-trabajadores",
+        icon: "",
+        title: "Servicios para trabajadores",
+        description: ""
+    },
+    {
+        id: 5.4,
+        category: "servicios-para-trabajadores",
+        icon: "",
+        title: "Servicios para trabajadores",
+        description: ""
+    },
+    {
+        id: 5.5,
+        category: "servicios-para-trabajadores",
+        icon: "",
+        title: "Servicios para trabajadores",
+        description: ""
+    },
+    {
+        id: 5.6,
+        category: "servicios-para-trabajadores",
+        icon: "",
+        title: "Servicios para trabajadores",
+        description: ""
+    },
+    // * ------ Servicios para Empresas ------
+    {
+        id: 6.1,
+        category: "servicios-para-empresas",
+        icon: "",
+        title: "Servicios para empresas",
+        description: ""
+    },
+    {
+        id: 6.2,
+        category: "servicios-para-empresas",
+        icon: "",
+        title: "Servicios para empresas",
+        description: ""
+    },
+    {
+        id: 6.3,
+        category: "servicios-para-empresas",
+        icon: "",
+        title: "Servicios para empresas",
+        description: ""
+    },
+    {
+        id: 6.4,
+        category: "servicios-para-empresas",
+        icon: "",
+        title: "Servicios para empresas",
+        description: ""
+    },
+    {
+        id: 6.5,
+        category: "servicios-para-empresas",
+        icon: "",
+        title: "Servicios para empresas",
+        description: ""
+    },
+    {
+        id: 6.6,
+        category: "servicios-para-empresas",
+        icon: "",
+        title: "Servicios para empresas",
+        description: ""
+    },
+    // * ------ Derecho Laboral ------
+    {
+        id: 7.1,
+        category: "derecho-laboral",
+        icon: "",
+        title: "Derecho Laboral",
+        description: ""
+    },
+    {
+        id: 7.2,
+        category: "derecho-laboral",
+        icon: "",
+        title: "Derecho Laboral",
+        description: ""
+    },
+    {
+        id: 7.3,
+        category: "derecho-laboral",
+        icon: "",
+        title: "Derecho Laboral",
+        description: ""
+    },
+    {
+        id: 7.4,
+        category: "derecho-laboral",
+        icon: "",
+        title: "Derecho Laboral",
+        description: ""
+    },
+    {
+        id: 7.5,
+        category: "derecho-laboral",
+        icon: "",
+        title: "Derecho Laboral",
+        description: ""
+    },
+    {
+        id: 7.6,
+        category: "derecho-laboral",
+        icon: "",
+        title: "Derecho Laboral",
+        description: ""
+    },
+    // * ------ Derecho Corporativo ------
+    {
+        id: 8.1,
+        category: "derecho-corporativo",
+        icon: "",
+        title: "Derecho Corporativo",
+        description: ""
+    },
+    {
+        id: 8.2,
+        category: "derecho-corporativo",
+        icon: "",
+        title: "Derecho Corporativo",
+        description: ""
+    },
+    {
+        id: 8.3,
+        category: "derecho-corporativo",
+        icon: "",
+        title: "Derecho Corporativo",
+        description: ""
+    },
+    {
+        id: 8.4,
+        category: "derecho-corporativo",
+        icon: "",
+        title: "Derecho Corporativo",
+        description: ""
+    },
+    {
+        id: 8.5,
+        category: "derecho-corporativo",
+        icon: "",
+        title: "Derecho Corporativo",
+        description: ""
+    },
+    {
+        id: 8.6,
+        category: "derecho-corporativo",
+        icon: "",
+        title: "Derecho Corporativo",
+        description: ""
+    },
+]
